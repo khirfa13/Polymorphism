4d28b05150eb3b0717febddecacc5fbc479127e1
khirfa13 <khoirul.rifai@students.amikom.ac.id>
2022-06-10 17:27:41 +0700

    Add project files.

---
+++ ConsoleApp2/PrinterWindows.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class PrinterWindows
+    {
+        public string Name { get; set; }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("Pilih Printer : ");
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine("Pilih Printer : ");
+        }
+    }
+}
